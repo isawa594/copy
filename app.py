d1566807fee7381b32ab2599591cd71c7fd897ac
isawa594 <144178877+isawa594@users.noreply.github.com>
2023-12-07 22:56:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ text_splitter = RecursiveCharacterTextSplitter(
 )
 
 st.title('デジタル推進タスク')
-st.title('_Streamlitでアプリ作ってみた_  :blue[アプリ] :')
+st.title('red[Streamlit]でアプリ作ってみた_  :blue[アプリ] :')
 
 
 option = st.selectbox(
